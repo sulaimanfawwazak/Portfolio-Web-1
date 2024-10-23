9149ff229270acb8f3b7f455fcbd63b2f515785f
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-10-23 15:59:31 +0700

    Feat: initialize React app and integrate it with TailwindCSS

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Nav Bar */}
+      {/* Hero */}
+      {/* About Me */}
+      {/* Skills */}
+      {/* Portfolios */}
+      {/* Companies */}
+      {/* Contact */}
+      {/* Footer */}
+      <h1 className="text-3xl font-bold underline">
+        Hello world!!!
+      </h1>
     </div>
   );
 }
